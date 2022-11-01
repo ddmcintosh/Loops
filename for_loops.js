a2d61649bbac0aa1ce17ecb561d84e19a715a8e1
Your Name <you@example.com>
2022-11-01 08:25:09 -0500

    Completed two exercises

---
+++ for_loops.js
@@ -0,0 +1,9 @@
+"use strict"
+
+let phrase = "I love loops";
+let i = 0;
+
+for (let i = 0; i < 7; i++) {
+    console.log(phrase);
+    
+}
---
+++ while_loops.js
@@ -0,0 +1,9 @@
+"use strict"
+
+let phrase = "I love loops";
+let i = 0;
+
+while (i<7) {
+console.log(phrase);
+i++;
+}
