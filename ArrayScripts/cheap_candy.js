d632bc6cc54425845017d853788d3eeed7436760
Your Name <you@example.com>
2022-11-01 14:02:13 -0500

    finished a next exercise

---
+++ ArrayScripts/cheap_candy.js
@@ -0,0 +1,11 @@
+"use strict"
+
+let products = [
+    { product: "Gummy Worms", price: 5.79},
+    { product: "Plain M&Ms", price: 2.89},
+    { product: "Peanut M&Ms", price: 2.89},
+    { product: "Swedish Fish", price: 3.79},
+];
+
+let numProducts = products.length;
+let cheapCandy
