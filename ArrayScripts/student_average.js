9612740de43a50fd9e813dc1fd7641a22ab21ff0
Your Name <you@example.com>
2022-11-01 13:19:26 -0500

    4th exercise completed

---
+++ ArrayScripts/student_average.js
@@ -0,0 +1,29 @@
+"use strict"
+
+let students = [
+    {name: "Zephaniah", scores: [100, 96, 99, 92] }, 
+    {name: "Pursalane", scores: [99, 89, 96, 100, 94] }, 
+    { name: "Siddalee", scores: [86, 72, 92] },
+    { name: "Ian", scores: [98, 84, 89, 100, 100, 76] },
+    { name: "Elisha", scores: [80, 100] },
+    { name: "Siddalee", scores: [86, 72, 92, 0, 0] },
+    { name: "Ezra", scores: [100, 99, 100, 87] },
+];
+
+let numStudents = students.length; 
+
+
+//outer loop goes through the array of students
+for (let i = 0; i < numStudents; i++) {
+    let total = 0; //initialized variables with values of 0 to make sure that they are number values but also to reset them to zero every time the outer loop runs
+    let avg = 0;
+    let numScores = students[i].scores.length;
+    //inner loop goes through the array of scores for each student
+    for (let j = 0; j < numScores; j++) {
+        total += students[i].scores[j];
+    }
+    avg += (total / numScores);//avg calculation done outside of inner loop once all the scores for student[i] are added to total
+    console.log(students[i].name + ": " + avg);//prints name of student with their average
+}
+
+
