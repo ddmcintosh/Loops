9312795ba7652d54b81acce70b4a82f80698de24
Your Name <you@example.com>
2022-11-01 09:19:27 -0500

    finished two exercises

---
+++ ArrayScripts/my_family.js
@@ -0,0 +1,11 @@
+"use strict"
+
+
+    let fam = ["Dave", "Nikki", "Me", "Kyori"];
+    let numMembers = fam.length;
+
+    for (let i = 0; i < numMembers; i++) {
+        console.log(fam[i]);
+    }
+
+
